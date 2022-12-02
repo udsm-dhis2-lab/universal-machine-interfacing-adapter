b55343f9954a6455b6e1ab6201a0c4290318ebc3
Bennett Benedict <bennybenester@gmail.com>
2022-12-02 17:54:42 +0300

    Add function response payloads

--- src/app/shared/interfaces/fx.interface.ts
+++ src/app/shared/interfaces/fx.interface.ts
@@ -4,7 +4,22 @@ export interface FxRequest {
   description: string;
   frequency: string;
 }
- export interface PageDetails{
+export interface PageDetails {
   page: number;
-  pageSize:number
- }
+  pageSize: number;
+}
+
+export interface FxPayload {
+  id: number;
+  code: string;
+  name: string;
+  description: string;
+  frequency: string;
+  count: string;
+  file?: File;
+}
+
+export interface FxResponse {
+  success: boolean;
+  message: string;
+}
