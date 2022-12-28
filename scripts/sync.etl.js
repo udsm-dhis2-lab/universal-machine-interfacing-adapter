fc98d36df78eb5d15614a26564497691902daf24
Bennett Benedict <bennybenester@gmail.com>
2022-12-28 15:39:32 +0300

    Add folder for scripts to send data to etl

---
+++ scripts/sync.etl.js
@@ -0,0 +1,35 @@
+
+const syncData = async (rows) => {
+    const url = `${context.secret.url}/api/trackedEntityInstances.json?filter=${context.secret.tbAttribute}:EQ:${rows[0].order_id}&ou=${context.secret.ou}&ouMode=DESCENDANTS&program=${context.secret.program}&fields=attributes[attribute,code,value],enrollments[*],orgUnit,trackedEntityInstance&paging=false`
+    const res = await context.http.get(url, {
+        auth: { username: context.secret.username, password: context.secret.password }, headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Credentials': 'true',
+            Accept: '*'
+        }
+    })
+    console.log(res.data)
+}
+const run = async () => {
+    try {
+        const db = new context.sqlite.Database(context.dbPath, context.sqlite.OPEN_READWRITE, (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+        });
+
+        const sql = `SELECT * FROM orders WHERE CAN_SYNC='true';`
+        db.all(sql, [], async (err, rows) => {
+            if (err) {
+                console.error(err);
+            } else {
+                await syncData(rows)
+            }
+        });
+
+        db.close();
+    } catch (e) { }
+}
+
+return run()
