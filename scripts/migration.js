f64a52b0ffd8be0f6077055760360968a229a0b0
Bennett Benedict <bennybenester@gmail.com>
2023-01-23 13:41:05 +0300

    Add migration function

---
+++ scripts/migration.js
@@ -0,0 +1,36 @@
+const closeDB = (sql, db) => {
+  db.all(sql, [], async (err, rows) => {
+    console.log('HERE', sql)
+    console.error(err);
+
+    console.log(rows);
+
+  });
+  db.close()
+}
+
+const start = async () => {
+  try {
+    const db = new context.sqlite.Database(
+      context.dbPath,
+      context.sqlite.OPEN_READWRITE,
+      (err) => {
+        if (err) {
+          console.error(err.message);
+        }
+      }
+    );
+
+    let sql = `ALTER TABLE PROCESS ADD COLUMN running BOOLEAN;`;
+    db.all(sql, [], async (err, rows) => {
+      sql = `UPDATE PROCESS SET RUNNING=${false} WHERE ID=${context.id}`
+      closeDB(sql, db)
+      console.error(err ? err : "No data to sync");
+
+      console.log('rows', rows);
+
+    });
+  } catch (e) { }
+};
+
+start();
