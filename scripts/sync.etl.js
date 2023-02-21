d1a0bd049dc782d15a1faf0cc3ca81bf2da20dfe
josephatJ <josephatjulius24@gmail.com>
2023-02-21 09:16:47 +0300

    Update etl sync function

--- scripts/sync.etl.js
+++ scripts/sync.etl.js
@@ -10,87 +10,90 @@ const syncData = async (rows) => {
     Accept: "*",
   };
   for (const row of rows) {
-    // Stage: yzu183PkJCH
-    console.log(row);
-    const url = `${context.secret.url}/api/trackedEntityInstances.json?filter=${context.secret.tbAttribute}:EQ:${row?.order_id}&ou=${context.secret.ou}&ouMode=DESCENDANTS&program=${context.secret.program}&fields=attributes[attribute,code,value],enrollments[*],orgUnit,trackedEntityInstance&paging=false`;
+    const url = `${context.secret.url}/api/trackedEntityInstances.json?filter=${
+      context.secret.tbAttribute
+    }:EQ:${row?.patient_id
+      .replace("-", "=")
+      .split("-")
+      .join("/")
+      .replace("=", "-")}&ou=${context.secret.ou}&ouMode=DESCENDANTS&program=${
+      context.secret.program
+    }&fields=attributes[attribute,code,value],enrollments[*],orgUnit,trackedEntityInstance&paging=false`;
     const res = await context.http.get(url, {
       auth,
       headers,
     });
     // FOUND CLIENTS
-    const trackedEntityInstanceData = res?.data?.trackedEntityInstances[0];
-    const eventPayload = {
-      orgUnit: trackedEntityInstanceData?.orgUnit,
-      program: "tj4u1ip0tTF",
-      programStage: "yzu183PkJCH",
-      status: "VISITED",
-      trackedEntityInstance: trackedEntityInstanceData?.trackedEntityInstance,
-      dataValues: [
-        {
-          dataElement: "LV7d7aBw0fn",
-          value: "true",
-          providedElsewhere: false,
-        },
-        {
-          dataElement: "jYKFZdR99Tz",
-          value: row?.order_id,
-          providedElsewhere: false,
-        },
-        {
-          dataElement: "i5OvzWAY8AK",
-          value: new Date(row?.created_at),
-          providedElsewhere: false,
-        },
-      ],
-    };
+    if (
+      res?.data?.trackedEntityInstances &&
+      res?.data?.trackedEntityInstances?.length > 0
+    ) {
+      const trackedEntityInstanceData = res?.data?.trackedEntityInstances[0];
+      const eventPayload = {
+        orgUnit: trackedEntityInstanceData?.orgUnit,
+        program: "tj4u1ip0tTF",
+        programStage: "yzu183PkJCH",
+        status: "VISITED",
+        trackedEntityInstance: trackedEntityInstanceData?.trackedEntityInstance,
+        dataValues: [
+          {
+            dataElement: "LV7d7aBw0fn",
+            value: "true",
+            providedElsewhere: false,
+          },
+          {
+            dataElement: "jYKFZdR99Tz",
+            value: row?.order_id,
+            providedElsewhere: false,
+          },
+          {
+            dataElement: "i5OvzWAY8AK",
+            value: new Date(row?.created_at),
+            providedElsewhere: false,
+          },
+        ],
+      };
 
-    const eventUrl = `${context.secret.url}/api/events${row?.reference_uuid ? "/" + row?.reference_uuid : ""
+      const eventUrl = `${context.secret.url}/api/events${
+        row?.reference_uuid ? "/" + row?.reference_uuid : ""
       }.json`;
-    const eventRes = (!row?.reference_uuid)
-      ? context.http.post(eventUrl, eventPayload, {
-        auth,
-        headers,
-      })
-      : context.http.put(eventUrl, eventPayload, {
-        auth,
-        headers,
-      });
-    const response = eventRes?.data?.response;
-    if (response?.imported === 1 || response?.updated === 1) {
-      // Went well
-      //   const sync_reference_payload = {
-      //     payload: eventPayload,
-      //     response: response,
-      //   };
-
-      //   const sync_reference = {
-      //     order_id: row?.id,
-      //     sync_reference: sync_reference_payload,
-      //   };
-      const reference_uuid = response?.importSummaries[0]?.reference;
-      const sync_status = response?.importSummaries[0]?.status;
-      //   Update the orders table for sync reference references
-      try {
-        const db = new context.sqlite.Database(
-          context.dbPath,
-          context.sqlite.OPEN_READWRITE,
-          (err) => {
-            if (err) {
-              console.error(err.message);
+      const eventRes = !row?.reference_uuid
+        ? context.http.post(eventUrl, eventPayload, {
+            auth,
+            headers,
+          })
+        : context.http.put(eventUrl, eventPayload, {
+            auth,
+            headers,
+          });
+      const response = eventRes?.data?.response;
+      if (response?.imported === 1 || response?.updated === 1) {
+        const reference_uuid = response?.importSummaries[0]?.reference;
+        const sync_status = response?.importSummaries[0]?.status;
+        //   Update the orders table for sync reference references
+        try {
+          const db = new context.sqlite.Database(
+            context.dbPath,
+            context.sqlite.OPEN_READWRITE,
+            (err) => {
+              if (err) {
+                console.error(err.message);
+              }
             }
-          }
-        );
+          );
 
-        const query = `UPDATE orders SET reference_uuid="${reference_uuid}",sync_status="${sync_status}" WHERE id=${row?.id}`;
-        db.all(query, [], async (err, rows) => {
-          if (!err) {
-            console.log(rows);
-          } else {
-            console.error(err);
-          }
-        });
-
-      } catch (e) { }
+          const query = `UPDATE orders SET reference_uuid="${reference_uuid}",sync_status="${sync_status}" WHERE id=${row?.id}`;
+          db.all(query, [], async (err, rows) => {
+            if (!err) {
+              console.log(rows);
+            } else {
+              console.error(err);
+            }
+          });
+        } catch (e) {}
+      } else {
+        window.alert(`NO client Found`);
+      }
     }
     // console.log(eventRes);
   }
@@ -105,7 +108,6 @@ const syncData = async (rows) => {
   });
 
   db.close();
-
 };
 const run = async () => {
   try {
@@ -127,8 +129,7 @@ const run = async () => {
         await syncData(rows);
       }
     });
-
-  } catch (e) { }
+  } catch (e) {}
 };
 
 return run();
